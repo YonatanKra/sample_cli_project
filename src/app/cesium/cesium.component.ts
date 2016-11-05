879d7937678d4e953f281be4664fb348b1689193
Yonatan <kra.yonatan@gmail.com>
2016-11-05 23:40:35 +0200

    Setting up cesium
    1) Added bing key
    2) Setup the CESIUM_BASE_URL to the assets/Cesium folder
    3) Initiate cesium viewer on component init
    4) Add the component to the view

--- src/app/cesium/cesium.component.ts
+++ src/app/cesium/cesium.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cesium',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CesiumComponent implements OnInit {
 
-  constructor() { }
+  cesiumViewer: any;
+
+  constructor(public element: ElementRef) {
+    Cesium.BingMapsApi.defaultKey = 'AroazdWsTmTcIx4ZE3SIicDXX00yEp9vuRZyn6pagjyjgS-VdRBfBNAVkvrucbqr';
+    (<any>window).CESIUM_BASE_URL = '/assets/Cesium';
+  }
 
   ngOnInit() {
+    this.cesiumViewer = new Cesium.Viewer(this.element.nativeElement.getElementsByClassName('cesium-container')[0]);
   }
 
 }
