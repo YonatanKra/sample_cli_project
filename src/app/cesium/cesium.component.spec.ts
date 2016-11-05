3addf6c755a20a129800f653aeaab5b2259bc986
Yonatan <kra.yonatan@gmail.com>
2016-11-05 23:28:22 +0200

    Generated cesium component
    `ng g component cesium`

---
+++ src/app/cesium/cesium.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CesiumComponent } from './cesium.component';
+
+describe('Component: Cesium', () => {
+  it('should create an instance', () => {
+    let component = new CesiumComponent();
+    expect(component).toBeTruthy();
+  });
+});
