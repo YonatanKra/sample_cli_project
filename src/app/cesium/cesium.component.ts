4a0cd714949584154f5b5433736249901701e79d
Yonatan <kra.yonatan@gmail.com>
2016-11-11 11:59:30 +0200

    Change Cesium component to work without DOM manipulation

--- src/app/cesium/cesium.component.ts
+++ src/app/cesium/cesium.component.ts
@@ -1,21 +1,28 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer, ViewChild } from '@angular/core';
 
+/**
+ * This is my cesium comment
+ */
 @Component({
   selector: 'app-cesium',
   templateUrl: './cesium.component.html',
   styleUrls: ['./cesium.component.css']
 })
 export class CesiumComponent implements OnInit {
-
+  @ViewChild('cesiumContainer') cesiumContainer: ElementRef;
   cesiumViewer: any;
 
-  constructor(public element: ElementRef) {
+  constructor(public element: ElementRef, private renderer: Renderer) {
     Cesium.BingMapsApi.defaultKey = 'AroazdWsTmTcIx4ZE3SIicDXX00yEp9vuRZyn6pagjyjgS-VdRBfBNAVkvrucbqr';
     (<any>window).CESIUM_BASE_URL = '/assets/Cesium';
   }
 
+  ngAfterViewInit() {
+    this.cesiumViewer = new Cesium.Viewer(this.cesiumContainer.nativeElement);
+  }
+
   ngOnInit() {
-    this.cesiumViewer = new Cesium.Viewer(this.element.nativeElement.getElementsByClassName('cesium-container')[0]);
+
   }
 
 }
